ead1d288556b549d3e0a773f812f87050f3c22a6
Arnab Ghorai <arnabghorai0154@gmail.com>
2024-02-25 17:53:19 +0530

    added localstorage

--- src/store/todo-store.jsx
+++ src/store/todo-store.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const TodoContext = createContext({
   intodo: [],
@@ -9,12 +9,16 @@ export const TodoContext = createContext({
 });
 
 function TodoContextProvider({ children }) {
-  const [intodo, setintodo] = useState([
-    { name: "incomp", date: "today", comp: false },
-  ]);
-  const [cotodo, setcotodo] = useState([
-    { name: "comp", date: "today", comp: true },
-  ]);
+  const [intodo, setintodo] = useState([]);
+  const [cotodo, setcotodo] = useState([]);
+
+  useEffect(() => {
+    let intodoStr = localStorage.getItem("intodo");
+    intodoStr ? setintodo(JSON.parse(localStorage.getItem("intodo"))) : null;
+
+    let cotodoStr = localStorage.getItem("cotodo");
+    cotodoStr ? setcotodo(JSON.parse(localStorage.getItem("cotodo"))) : null;
+  }, []);
 
   const handleNewItem = (newName, newDate) => {
     if (newName !== "" && newDate !== "") {
@@ -23,6 +27,7 @@ function TodoContextProvider({ children }) {
         ...intodo,
       ];
       setintodo(newintodo);
+      localStorage.setItem("intodo", JSON.stringify(newintodo));
     }
   };
 
@@ -31,10 +36,12 @@ function TodoContextProvider({ children }) {
       const newTodo = [...cotodo];
       newTodo.splice(index, 1);
       setcotodo(newTodo);
+      localStorage.setItem("cotodo", JSON.stringify(newTodo));
     } else {
       const newTodo = [...intodo];
       newTodo.splice(index, 1);
       setintodo(newTodo);
+      localStorage.setItem("intodo", JSON.stringify(newTodo));
     }
   };
 
@@ -43,22 +50,26 @@ function TodoContextProvider({ children }) {
       const newCotodo = [...cotodo];
       newCotodo.splice(index, 1);
       setcotodo(newCotodo);
+      localStorage.setItem("cotodo", JSON.stringify(newCotodo));
 
       const newIntodo = [
         ...intodo,
         { name: swapTodo.name, date: swapTodo.date, comp: false },
       ];
       setintodo(newIntodo);
+      localStorage.setItem("intodo", JSON.stringify(newIntodo));
     } else {
       const newIntodo = [...intodo];
       newIntodo.splice(index, 1);
       setintodo(newIntodo);
+      localStorage.setItem("intodo", JSON.stringify(newIntodo));
 
       const newCotodo = [
         { name: swapTodo.name, date: swapTodo.date, comp: true },
         ...cotodo,
       ];
       setcotodo(newCotodo);
+      localStorage.setItem("cotodo", JSON.stringify(newCotodo));
     }
   };
 
